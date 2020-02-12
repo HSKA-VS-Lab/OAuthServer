bc632d916f275ae7192cddf5291aac6cfde7b068
BobFlop <BobFlop@users.noreply.github.com>
2020-02-12 21:59:17 +0100

    ES GEHT!!!

--- src/main/java/de/hska/iwi/vslab/OAuthServer/WebSecurityConfig.java
+++ src/main/java/de/hska/iwi/vslab/OAuthServer/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package de.hska.iwi.vslab.OAuthServer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
